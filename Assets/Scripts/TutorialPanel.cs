6c1a5af1a2d1b0b4538ede8751e960cb7c7b3887
Mister M. <fnaf.bonni@bk.ru>
2025-03-28 19:46:01 +0400

    Added tutorial panel localization

---
+++ Assets/Scripts/TutorialPanel.cs
@@ -0,0 +1,63 @@
+using System;
+using DG.Tweening;
+using UnityEngine;
+using UnityEngine.UI;
+using YG;
+
+public class TutorialPanel : MonoBehaviour
+{
+    [SerializeField] private CanvasGroup canvasGroup;
+    [SerializeField] private Image tutorialPageImage;
+    [SerializeField] private Sprite enSprite;
+    [SerializeField] private Sprite ruSprite;
+
+    private void Start()
+    {
+        if (YG2.isFirstGameSession)
+        {
+            Open();
+        }
+    }
+
+    public void Open()
+    {
+        DOTween.Sequence()
+            .Append(canvasGroup.DOFade(1.0f, 0.2f))
+            .AppendCallback(() => canvasGroup.interactable = true)
+            .AppendCallback(() => canvasGroup.blocksRaycasts = true)
+            .Play();
+    }
+    
+    public void Close()
+    {
+        DOTween.Sequence()
+            .Append(canvasGroup.DOFade(0.0f, 0.2f))
+            .AppendCallback(() => canvasGroup.interactable = false)
+            .AppendCallback(() => canvasGroup.blocksRaycasts = false)
+            .Append(transform.DOScale(0.9f, 0.15f))
+            .Append(transform.DOScale(1.0f, 0.15f))
+            .Play();
+    }
+    
+    private void OnLanguageSwitched(string language)
+    {
+        if (language == "ru")
+        {
+            tutorialPageImage.sprite = ruSprite;
+        }
+        else if (language == "en")
+        {
+            tutorialPageImage.sprite = enSprite;
+        }
+    }
+    
+    private void OnEnable()
+    {
+        YG2.onSwitchLang += OnLanguageSwitched;
+    }
+    
+    private void OnDisable()
+    {
+        YG2.onSwitchLang -= OnLanguageSwitched;
+    }
+}
