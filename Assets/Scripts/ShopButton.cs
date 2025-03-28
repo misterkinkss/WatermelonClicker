d4ba827c5c2551cfa34e0d46f74d6b44da61de34
MagMasterixUps <139586610+MagMasterixUps@users.noreply.github.com>
2025-03-28 20:07:09 +0400

    Merge pull request #3 from MagMasterixUps

--- Assets/Scripts/ShopButton.cs
+++ Assets/Scripts/ShopButton.cs
@@ -1,16 +1,18 @@
 using DG.Tweening;
 using TMPro;
 using UnityEngine;
+using UnityEngine.UI;
 
 public abstract class ShopButton : MonoBehaviour
 {
-    [SerializeField] protected ParticleSystem buyParticleSystem;
-    [SerializeField] protected TMP_Text buyParticleText;
+    [SerializeField] private ParticleSystem buyParticleSystem;
+    [SerializeField] private TMP_Text buyParticleText;
+    [SerializeField] private AudioSource audioSource;
+    [SerializeField] private Image image;
+    
     [SerializeField] protected ScoreCounter scoreCounter;
-    [SerializeField] protected AudioSource audioSource;
     [SerializeField] protected double price;
     
-
     public virtual void Buy()
     {
         DOTween.Sequence()
@@ -22,5 +24,20 @@ public abstract class ShopButton : MonoBehaviour
 
         buyParticleText.text = "-" + ScoreFormatter.Format(price);
         buyParticleSystem.Emit(1);
+    }
+
+    private void OnScoreChanged(double score)
+    {
+        image.color = score >= price ? Color.white : Color.gray;
+    }
+
+    private void OnEnable()
+    {
+        ScoreCounter.OnScoreChanged += OnScoreChanged;
+    }
+
+    private void OnDisable()
+    {
+        ScoreCounter.OnScoreChanged -= OnScoreChanged;
     }
 }
