dba649c80e90bcf57ffa9549e636a3e9f7733b6a
Mister M. <fnaf.bonni@bk.ru>
2025-03-30 02:15:49 +0400

    Connected leaderboard system

--- Assets/Scripts/LeaderboardManager.cs
+++ Assets/Scripts/LeaderboardManager.cs
@@ -1,6 +1,32 @@
+using System.Collections;
 using UnityEngine;
+using YG;
 
 public class LeaderboardManager : MonoBehaviour
 {
-    
+    [SerializeField] private ScoreCounter scoreCounter;
+
+    private void Start()
+    {
+        StartCoroutine(LeaderboardUpdateCycle());
+    }
+
+    private IEnumerator LeaderboardUpdateCycle()
+    {
+        WaitForSeconds waitForSeconds = new WaitForSeconds(3.0f);
+
+        double maxScore = 0.0D;
+        
+        while (true)
+        {
+            yield return waitForSeconds;
+
+            if (YG2.isSDKEnabled && scoreCounter.Score > maxScore) 
+            {
+                YG2.SetLeaderboard("watermelonBoard", (int) maxScore);
+                
+                maxScore = scoreCounter.Score;
+            }
+        }
+    }
 }
